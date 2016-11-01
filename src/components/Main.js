dac7e021bb1b32c51391d85724990e6bceb68cd0
Perter <447130707@qq.com>
2016-11-01 09:37:02 +0800

    主脚本文件提交

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,26 @@ import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
 
+class NavComponent extends  React.Component {
+    render(){
+        return (
+            this.props.Nav.map(function (item) {
+                <li><a href={item.url}>{item.text}</a></li>
+            })
+        );
+    }
+}
+
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
+        <div className="head">
+            <a href="/" class="logo"><img src={this.props.logo.url} alt="log"/></a>
+            <ul>
+                <NavComponent Nav={this.props.Nav}/>
+            </ul>
+        </div>
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
@@ -17,6 +33,25 @@ class AppComponent extends React.Component {
 }
 
 AppComponent.defaultProps = {
+    logo:{
+        url:'http://upload.youbika666.com/Web/images/default/logo.png'
+    },
+    Nav: [{
+        url:'/',
+        text:'首页'
+    },{
+        url:'/a',
+        text:'文展'
+    },{
+        url:'/t',
+        text:'图展'
+    },{
+        url:'/j',
+        text:'技展'
+    }]
+}
+NavComponent.defaultProps = {
+
 };
 
 export default AppComponent;
